0ce141ea809440b3e45ab30c28c78fb1f77d502e
buildautomation <buildautomation@syncfusion.com>
2021-01-28 13:42:27 +0530

    v18.4.0.39

--- scripts/jsongridmerge.js
+++ scripts/jsongridmerge.js
@@ -1,6 +1,6 @@
 /*!
 *  filename: jsondata.min.js
-*  version : 11.4*  Copyright Syncfusion Inc. 2001 - 2020. All rights reserved.
+*  version : 11.4*  Copyright Syncfusion Inc. 2001 - 2021. All rights reserved.
 *  Use of this code is subject to the terms of our license.
 *  A copy of the current license can be obtained at any time by e-mailing
 *  licensing@syncfusion.com. Any infringement will be prosecuted under
